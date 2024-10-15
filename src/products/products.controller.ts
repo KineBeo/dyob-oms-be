930c885ea18235ba7edccdff10d1a6c52aa07f70
TruongHoang2004 <22028035@vnu.edu.vn>
2024-10-15 11:16:22 +0700

    feat: create service function for product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,33 +1,77 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import Product from './entities/product.entity';
 
 @Controller('products')
+@ApiTags('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a product' })
+  @ApiResponse({
+    status: 201,
+    description: 'The product has been successfully created.',
+    type: CreateProductDto,
+  })
+  @ApiResponse({ status: 404, description: 'Category not found.' })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all products' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return all products.',
+    type: Product,
+  })
   findAll() {
     return this.productsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Find a product by id' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return the product.',
+    type: Product,
+  })
+  @ApiResponse({ status: 404, description: 'Product not found.' })
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a product by id' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return the updated product.',
+    type: Product,
+  })
+  @ApiResponse({ status: 404, description: 'Product not found.' })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a product by id' })
+  @ApiResponse({
+    status: 200,
+    description: 'The product has been successfully deleted.',
+  })
+  @ApiResponse({ status: 404, description: 'Product not found.' })
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
   }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import Product from './entities/product.entity';
+import { Repository } from 'typeorm';
+import ProductCategory from './entities/product_category.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+    @InjectRepository(ProductCategory)
+    private productCategoryRepository: Repository<ProductCategory>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto): Promise<Product> {
+    const category = this.productCategoryRepository.findOne({
+      where: { id: createProductDto.category_id },
+    });
+    if (!category) {
+      throw new NotFoundException(
+        `Category #${createProductDto.category_id} not found`,
+      );
+    }
+
+    const product = this.productRepository.create(createProductDto);
+    return await this.productRepository.save(product);
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(): Promise<Product[]> {
+    return await this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number): Promise<Product> {
+    const product = await this.productRepository.findOne({ where: { id } });
+    if (!product) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const { category_id, ...rest } = updateProductDto;
+    const category = await this.productCategoryRepository.findOne({
+      where: { id: category_id },
+    });
+
+    if (!category) {
+      throw new NotFoundException(`Category #${category_id} not found`);
+    }
+
+    const product = await this.productRepository.findOne({ where: { id } });
+    if (!product) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+
+    Object.assign(product, rest);
+    return this.productRepository.save(product);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} product`;
+    if (!this.productRepository.findOne({ where: { id } })) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+
+    return this.productRepository.delete(id);
   }
 }
