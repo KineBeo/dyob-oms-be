1864865b252dba19114499ee079f1f770a230318
TruongHoang2004 <22028035@vnu.edu.vn>
2025-02-10 16:19:38 +0700

    feat: total transaction analysis

--- src/analysis/analysis.controller.ts
+++ src/analysis/analysis.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { AnalysisService } from './analysis.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { AdminGuard } from 'src/auth/guards/admin.guard';
 
 @Controller('analysis')
 @ApiTags('Analysis')
 export class AnalysisController {
   constructor(private readonly analysisService: AnalysisService) {}
 
-  @Get()
+  @Get('user')
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth('JWT-auth')
   async getAnalysisForUser(
@@ -17,5 +18,19 @@ export class AnalysisController {
     @Req() req,
   ) {
     return this.analysisService.getAnalysisForUser(month, year, req.user.sub);
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard, AdminGuard)
+  @ApiBearerAuth('JWT-auth')
+  @ApiQuery({
+    name: 'start',
+    required: true,
+    example: '2021-01-01',
+    type: Date,
+  })
+  @ApiQuery({ name: 'end', required: true, example: '2021-01-31', type: Date })
+  async getAnalysis(@Query('start') start: Date, @Query('end') end: Date) {
+    return this.analysisService.getAnalysisForAllUsers(start, end);
   }
 }
--- src/analysis/analysis.service.ts
+++ src/analysis/analysis.service.ts
@@ -71,4 +71,96 @@ export class AnalysisService {
       },
     };
   }
+
+  async getAnalysisForAllUsers(startDate: Date, endDate: Date) {
+    const userTransactions = await this.userTransactionRepository
+      .createQueryBuilder('transaction')
+      .select("to_char(transaction.createdAt, 'YYYY-MM-DD')", 'date')
+      .addSelect(
+        `SUM(CASE WHEN transaction.type = 'COMMISSION' THEN CAST(transaction.amount as DECIMAL) ELSE 0 END)`,
+        'commission_amount',
+      )
+      .addSelect(
+        `SUM(CASE WHEN transaction.type = 'BONUS' THEN CAST(transaction.amount as DECIMAL) ELSE 0 END)`,
+        'bonus_amount',
+      )
+      .addSelect(
+        `SUM(CASE WHEN transaction.type = 'PURCHASE' THEN CAST(transaction.amount as DECIMAL) ELSE 0 END)`,
+        'purchase_amount',
+      )
+      .addSelect(
+        `SUM(CASE WHEN transaction.type = 'SALE' THEN CAST(transaction.amount as DECIMAL) ELSE 0 END)`,
+        'sale_amount',
+      )
+      .addSelect(
+        `COUNT(CASE WHEN transaction.type = 'COMMISSION' THEN 1 END)`,
+        'commission_count',
+      )
+      .addSelect(
+        `COUNT(CASE WHEN transaction.type = 'BONUS' THEN 1 END)`,
+        'bonus_count',
+      )
+      .addSelect(
+        `COUNT(CASE WHEN transaction.type = 'PURCHASE' THEN 1 END)`,
+        'purchase_count',
+      )
+      .addSelect(
+        `COUNT(CASE WHEN transaction.type = 'SALE' THEN 1 END)`,
+        'sale_count',
+      )
+      .where('transaction.createdAt >= :startDate', { startDate })
+      .andWhere('transaction.createdAt <= :endDate', { endDate })
+      .groupBy("to_char(transaction.createdAt, 'YYYY-MM-DD')")
+      .orderBy('date', 'ASC')
+      .getRawMany();
+
+    const total = userTransactions.reduce(
+      (acc, curr) => {
+        acc.commission_amount = String(
+          Number(acc.commission_amount) + Number(curr.commission_amount),
+        );
+        acc.bonus_amount = String(
+          Number(acc.bonus_amount) + Number(curr.bonus_amount),
+        );
+        acc.purchase_amount = String(
+          Number(acc.purchase_amount) + Number(curr.purchase_amount),
+        );
+        acc.sale_amount = String(
+          Number(acc.sale_amount) + Number(curr.sale_amount),
+        );
+        acc.commission_count = String(
+          Number(acc.commission_count) + Number(curr.commission_count),
+        );
+        acc.bonus_count = String(
+          Number(acc.bonus_count) + Number(curr.bonus_count),
+        );
+        acc.purchase_count = String(
+          Number(acc.purchase_count) + Number(curr.purchase_count),
+        );
+        acc.sale_count = String(
+          Number(acc.sale_count) + Number(curr.sale_count),
+        );
+        return acc;
+      },
+      {
+        commission_amount: 0,
+        bonus_amount: 0,
+        purchase_amount: 0,
+        sale_amount: 0,
+        commission_count: 0,
+        bonus_count: 0,
+        purchase_count: 0,
+        sale_count: 0,
+      },
+    );
+
+    return {
+      userTransactions,
+      period: {
+        startDate,
+        endDate,
+      },
+      total,
+    };
+  }
 }
